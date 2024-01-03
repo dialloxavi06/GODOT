5f90c5f649c6231f3d42da3ce266189d4295e380
dialloxavi06 <107733204+dialloxavi06@users.noreply.github.com>
2024-01-03 15:50:56 +0100

    Update crate.gd

--- Scripts/Detectable/crate.gd
+++ Scripts/Detectable/crate.gd
@@ -9,7 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+ 
 
 func _on_hitbox_area_entered(area):
 	if area.is_in_group("Sword"):
